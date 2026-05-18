7ca08409cc0c5cccb07d276ab364e409b02464e9
JahirManzaba <jahir.manzaba@udla.edu.ec>
2026-05-18 12:20:27 -0500

    version final

--- ejercicio1.c
+++ ejercicio1.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
-float calcularPromedio (float *vector,int tamanio)
-int main() {
+float calcularPromedio (float *vector,int tamanio);
+int main()
+{
     float notas[5]={6,7,7.5,6.5,8} ;
-    int tamanio=5;
+    int tamanio = 5;
     for (int i = 0; i < 5; i++)
     {
         printf("%.2f ", notas[i]);
     }
-    
-    calcularPromedio(notas, tamanio);
+      calcularPromedio(notas, tamanio);
+}
 
-
+   float calcularPromedio(float *vector, int tamanio) 
+{
+    float suma, promedio=0;
+    for (int i = 0; i < tamanio; i++)
+    {
+        suma = suma + vector[i];
+    }
+    promedio = suma / tamanio;
+    return promedio;
 }
